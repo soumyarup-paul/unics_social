8bd176a96e0e32e4a669921413182caa10a4da97
Amish Shah <amishshah.2k@gmail.com>
2020-09-27 13:45:49 +0100

    chore: trigger Netlify deploy

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Countdown from './Countdown';
 
+// Stop peeking >:(
 ReactDOM.render(
 	<Countdown />,
 	document.getElementById('root')
